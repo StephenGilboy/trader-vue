106f41d6d5c415264ed05c0d91e585e314d66c23
Stephen Gilboy <sgilboy@outlook.com>
2017-01-14 11:42:05 -0800

    Display ripple effect on row that changes color depending on movement

--- src/components/quoteBook/Quote.vue
+++ src/components/quoteBook/Quote.vue
@@ -1,6 +1,6 @@
 <template>
-  <md-table-row>
-    <md-table-cell>{{quote.symbol}}</md-table-cell>
+  <md-table-row class="has-ripple">
+    <md-table-cell>{{quote.symbol}} <md-ink-ripple :class="move" /></md-table-cell>
     <md-table-cell>{{quote.bidSize}}</md-table-cell>
     <md-table-cell>{{quote.bid}}</md-table-cell>
     <md-table-cell>{{quote.ask}}</md-table-cell>
@@ -14,11 +14,57 @@
 <script>
   export default {
     props: ['quote'],
+    data () {
+      return {
+        move: 'move-flat'
+      }
+    },
     methods: {
       notifyUnsubscribe: function () {
         this.$emit('unsubscribe', this.quote);
       }
+    },
+    computed: {
+      last: function () {
+        return this.quote.last;
+      }
+    },
+    watch: {
+      last: function (nv, ov) {
+        const lastMove = this.move;
+        if (nv > ov) {
+          this.move = 'move-up';
+        } else if (nv < ov) {
+          this.move = 'move-down';
+        } else {
+          this.move = 'move-flat';
+        }
+        if (lastMove !== this.move) {
+          const quoteLastCell = this.$el.children[0].children[0];
+          const x = quoteLastCell.getBoundingClientRect().left + 25;
+          const y = quoteLastCell.getBoundingClientRect().top + 15;
+          // eslint-disable-next-line no-undef
+          let e = new MouseEvent('mousedown', {
+            'bubbles': true,
+            'cancelable': true,
+            'pageX': x,
+            'pageY': y,
+            'clientX': x,
+            'clientY': y,
+            'screenX': x,
+            'screenY': y
+          });
+          quoteLastCell.dispatchEvent(e);
+        }
+      }
     }
   }
 </script>
-<style></style>
+<style>
+  .move-up {
+    color: green;
+  }
+  .move-down {
+    color: red;
+  }
+</style>
